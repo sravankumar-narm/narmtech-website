abf40b272e5896eaf3f605ee692b74c1a38e46da
sravankumar-narm <150946151+sravankumar-narm@users.noreply.github.com>
2024-02-05 13:51:51 +0530

    new code updated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './componnets/Home'
+
+function App() {
+  return (
+    <div className="container-fuild">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
